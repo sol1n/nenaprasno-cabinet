a32d1fb236ded06b86e29e08742abdc2376c2787
tsyrya <tsyrya@gmail.com>
2017-12-25 22:11:25 +0800

    test cors headers

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,9 @@ Route::post('/login', 'AuthController@ProcessLogin');
 Route::post('/registration', 'AuthController@ProcessRegistration');
 Route::post('/loginByToken', 'AuthController@LoginByToken');
 Route::options('/loginByToken', 'AuthController@LoginByTokenOptions');
-Route::post('/loginBySocial', 'AuthController@LoginBySocial')->middleware('Cors');
+Route::group(['middleware' => ['Cors']], function () {
+    Route::post('/loginBySocial', 'AuthController@LoginBySocial');
+});
 
 
 Route::group(['middleware' => ['appercodeAuth']], function () {
