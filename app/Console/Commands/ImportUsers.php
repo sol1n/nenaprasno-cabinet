bb61f6e950cbfed8701ba8ec28e76c8a9c87b001
tsyrya <tsyrya@gmail.com>
2017-12-27 21:10:13 +0800

    import users command

---
+++ app/Console/Commands/ImportUsers.php
@@ -0,0 +1,152 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Backend;
+use App\Exceptions\User\UserNotFoundException;
+use App\Object;
+use App\Services\ObjectManager;
+use App\Services\SchemaManager;
+use App\User;
+use Illuminate\Console\Command;
+use Illuminate\Pagination\Paginator;
+use Mockery\Exception;
+
+class ImportUsers extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'import:users';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Import users';
+
+    protected $backend;
+
+    private function initBackend()
+    {
+        $this->backend = new Backend(env('APPERCODE_DEFAULT_BACKEND'), env('APPERCODE_SERVER'));
+        $user = User::Login($this->backend, [
+            'login' => env('APPERCODE_LOGIN'),
+            'password' => env('APPERCODE_PASSWORD')
+        ], false);
+        $this->backend->token = $user->token();
+        app()->instance(Backend::class, $this->backend);
+    }
+
+    /**
+     * @param int $length
+     * @return bool|string
+     */
+    function random_password( $length = 8 ) {
+        $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$";
+        $password = substr( str_shuffle( $chars ), 0, $length );
+        return $password;
+    }
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->initBackend();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $objectManager = new ObjectManager();
+        $schemaManager = new SchemaManager();
+//        $file = file_get_contents(base_path('vagrant/1.txt'));
+//        $ids = explode("\n", $file);
+
+        $schema = $schemaManager->find('UserProfiles');
+
+//        foreach ($ids as $id) {
+//            if ($id) {
+//                try {
+//                    $user = User::get($id, $this->backend);
+//                    if ($user) {
+//                        $profile = Object::list($schema, $this->backend, ['search' => ['userId' => $user->id]]);
+//                        if ($profile->count() > 0) {
+//                            $profile->first()->delete($this->backend);
+//                        }
+//                        $user->delete($this->backend);
+//                        $this->info($user->id);
+//                    }
+//                }
+//                catch (UserNotFoundException $e) {
+//
+//                }
+//            }
+//        }
+//        die('done');
+
+
+        $regions = $objectManager->search($schemaManager->find('Region'), ['take' => -1])->map(function($item) {
+            return [
+                'id' => $item->id,
+                'title' => isset($item->fields['title']) ? $item->fields['title'] : ''
+            ];
+        });
+
+        $regions = collect($regions);
+
+        $this->info('start');
+        $file = file_get_contents(base_path('vagrant/NNP3_CLIENT.csv'));
+        $rows = explode("\r\n", $file);
+        unset($rows[0]);
+        foreach ($rows as $index => $row) {
+           if ($row) {
+               $item = explode(';', $row);
+               $fio = str_replace('"','', $item[4]);
+               $phone = str_replace('"','', $item[8]);
+               $email = str_replace('"','',$item[9]);
+               $region = str_replace('"','',$item[16]);
+
+               $regionObject = ($regions->filter(function($value, $key) use($region) {
+                   return mb_stripos($value['title'], $region) !== false;
+               }));
+
+               $user = User::create([
+                   'username' => $email,
+                   'password' => $this->random_password()
+               ], $this->backend);
+
+               if ($user->id) {
+                   $data = [
+                       'userId' => $user->id,
+                       'email' => $email,
+                       'phoneNumber' => $phone ?? '',
+                       'lastName' => $fio ?? ''
+                   ];
+
+                   if ($regionObject->count() > 0) {
+                       $data['regionId'] = $regionObject->first()['id'];
+                   }
+
+                   $objectManager->create($schema, $data);
+               }
+               $this->info($user->id);
+
+               file_put_contents(base_path('vagrant/1.txt'), $user->id . "\n", FILE_APPEND);
+           }
+        }
+        $this->info('done');
+    }
+}
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\ImportUsers;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -13,7 +14,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-      Commands\Notification::class
+      Commands\Notification::class,
+        ImportUsers::class
     ];
 
     /**
