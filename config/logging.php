93f7e5f6e4697ce720fe9bd92514156665cbb10a
Artem Garnyshev <is.perfect.possible@gmail.com>
2018-04-18 17:37:19 +0700

    Slack logging

--- config/logging.php
+++ config/logging.php
@@ -51,9 +51,9 @@ return [
         'slack' => [
             'driver' => 'slack',
             'url' => env('LOG_SLACK_WEBHOOK_URL'),
-            'username' => 'Laravel Log',
-            'emoji' => ':boom:',
-            'level' => 'critical',
+            'username' => 'Nenaprasno cabinet',
+            'emoji' => ':pill:',
+            'level' => 'debug',
         ],
 
         'syslog' => [
